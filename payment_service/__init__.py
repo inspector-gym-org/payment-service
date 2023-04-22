270a5f9b5d8bafd53899c7c047ab9cc60777e6a9
Andrew Yatsura <andrewyazura203@gmail.com>
2023-04-22 04:16:59 +0300

    add fastapi app factory

---
+++ payment_service/__init__.py
@@ -0,0 +1,21 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from fastapi import FastAPI
+
+from .config import settings
+from .database import create_indexes
+from .routes import router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    await create_indexes()
+    yield
+
+
+def build_app() -> FastAPI:
+    app = FastAPI(root_path=settings.app_root_path, lifespan=lifespan)
+    app.include_router(router)
+
+    return app
